f31e56e258f98c876c1a5f04ed62451c8c4d7276
sfechner <sfechner@stanford.edu>
2020-04-14 22:22:36 -0700

    Update SFCircuit.py
    added missing braket

--- SFCircuit.py
+++ SFCircuit.py
@@ -36,7 +36,7 @@ def resource_path(relative_path):
     except Exception:
         base_path = os.path.abspath(".")
 
-    return os.path.join(base_path, relative_path
+    return os.path.join(base_path, relative_path)
 
 
 def time_as_int():
